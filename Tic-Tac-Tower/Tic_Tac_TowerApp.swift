6f9c7b3599848ad969a332da84e89887f0e07a99
Eugene Wang <wangc@wangc.com>
2024-07-08 11:15:51 -0700

    Initial Commit

---
+++ Tic-Tac-Tower/Tic_Tac_TowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_TowerApp.swift
+//  Tic-Tac-Tower
+//
+//  Created by Eugene Wang on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_TowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
